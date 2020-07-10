7a95554bed6c135e0871783a0b97ddfb7912a495
Brian Pham <quocthang_pham@singaporeair.com.sg>
2020-07-10 17:35:18 +0800

    update configs

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const debug = require('debug')('test:app')
+const appInsight = require('applicationinsights')
+appInsight.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+appInsight.start()
 const server = http.createServer((request, response) => {
     debug('started')
+    console.log('started')
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 });
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || process.env.port || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
