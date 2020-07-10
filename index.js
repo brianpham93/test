18d2cc81ef1b6a455221acc5d901651efa9f4e06
Brian Pham <quocthang_pham@singaporeair.com.sg>
2020-07-10 16:49:16 +0800

    Init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
-
+const debug = require('debug')('test:app')
 const server = http.createServer((request, response) => {
+    debug('started')
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 });
