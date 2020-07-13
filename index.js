ef9233d2feaf91100c4357d336cb7afdb0342c30
Brian Pham <quocthang_pham@singaporeair.com.sg>
2020-07-13 17:33:16 +0800

    test assert

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const util = require('util');
 const logger = require('./logger')
-
+const oidc = require('oidc-token-hash')
 const appInsight = require('applicationinsights')
-appInsight.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+const { strict: assert } = require('assert');
+
+appInsight.setup('e2a2a87b-da65-4e7d-a927-3bf4aa6af0d3')
 .setAutoDependencyCorrelation(true)
 .setAutoCollectRequests(true)
 .setAutoCollectPerformance(true, true)
@@ -14,13 +16,15 @@ appInsight.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
 .setDistributedTracingMode(appInsight.DistributedTracingModes.AI)
 appInsight.start()
 const server = http.createServer((request, response) => {
+    assert(typeof request === 'string' && request, 'request is not a string')
     logger.info('Request received')
-    logger.debug('Request: ', request)
+    logger.debug('Request: ', request.headers)
+    logger.error('Some error')
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 });
 
 const port = process.env.PORT || process.env.port || 1337;
 server.listen(port);
-
+console.log(oidc.generate('yEYxAB5vOTMW87HGeS8nM161ApsBCWFPfeP8W3IAg0AIdk4yBGdo2oozASLCKokn8nTRYzUtr9U6vcHH', 'RS256'))
 console.log("Server running at http://localhost:%d", port);
